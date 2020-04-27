40e6a56e0b899d97852005e36eedeea9f59ead3f
Thiago <thiago.ferraz@dbccompany.com.br>
2020-04-27 18:04:39 -0300

    ajust unique

--- src/infra/database/migrations/20200413163120_company.js
+++ src/infra/database/migrations/20200413163120_company.js
@@ -2,13 +2,14 @@ exports.up = function (knex) {
     return knex.schema.createTable("company", function (table) {
         table.increments();
         table.string("name").notNullable();
-        table.string("cnpj").notNullable();
+        table.integer("cnpj").notNullable();
         table.string("dtNascimento").notNullable();
         table.string("sexo").notNullable();
         table.string("email").notNullable();
         table.boolean("active").notNullable();
         table.integer("user_id").notNullable();
         table.foreign("user_id").references("id").inTable("user");
+        table.unique("cnpj");
     });
 };
 
--- src/infra/database/migrations/20200413163319_people.js
+++ src/infra/database/migrations/20200413163319_people.js
@@ -2,13 +2,14 @@ exports.up = function (knex) {
     return knex.schema.createTable("people", function (table) {
         table.increments();
         table.string("name").notNullable();
-        table.string("cpf").notNullable();
+        table.integer("cpf").notNullable();
         table.string("dtNascimento").notNullable();
         table.string("sexo").notNullable();
         table.string("email").notNullable();
         table.string("active").notNullable();
         table.integer("user_id").notNullable();
         table.foreign("user_id").references("id").inTable("user");
+        table.unique("cpf");
     });
 };
 

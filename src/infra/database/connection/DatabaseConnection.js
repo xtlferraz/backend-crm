25710903e8a9e0050be3290605b497b10139b5ea
Thiago <thiago.ferraz@dbccompany.com.br>
2020-04-27 11:42:00 -0300

    conecction prod

--- src/infra/database/connection/DatabaseConnection.js
+++ src/infra/database/connection/DatabaseConnection.js
@@ -1,4 +1,4 @@
 const knex = require("knex");
 const configuration = require("../../../../knexfile");
 
-module.exports = knex(configuration.development);
+module.exports = knex(configuration.production);

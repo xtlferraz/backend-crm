d251ca5541d2ed3196cc2502cca02b8874702efc
Thiago <thiago.ferraz@dbccompany.com.br>
2020-05-26 12:06:12 -0300

    ajust em container

---
+++ container.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { createContainer, InjectionMode, asValue } = require("awilix");
+const app = express();
+const createError = require("http-errors");
+const axios = require("axios");
+const connection = require("./src/infra/database/connection/DatabaseConnection");
+const jwt = require("jsonwebtoken");
+const config = "security-auth";
+const bcrypt = require("bcrypt");
+const container = createContainer();
+container
+    .register({
+        createError: asValue(createError),
+        connection: asValue(connection),
+        axios: asValue(axios),
+        jwt: asValue(jwt),
+        config: asValue(config),
+        bcrypt: asValue(bcrypt),
+        app: asValue(app),
+        express: asValue(express),
+    })
+    .loadModules(
+        [
+            "src/app/operations/**/*.js",
+            "src/app/services/**/*.js",
+            "src/infra/database/repository/**/*.js",
+            "src/infra/integration/rest/**/*.js",
+        ],
+        {
+            formatName: "camelCase",
+            resolverOptions: {
+                injectionMode: InjectionMode.PROXY,
+            },
+        }
+    );
+module.exports = container;
--- index.js
+++ index.js
@@ -1,40 +1,9 @@
-const express = require("express");
-const { createContainer, InjectionMode, asValue } = require("awilix");
 const { loadControllers, scopePerRequest } = require("awilix-express");
-const app = express();
-const createError = require("http-errors");
-const axios = require("axios");
-const connection = require("./src/infra/database/connection/DatabaseConnection");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const config = "security-auth";
 const morgan = require("morgan");
-const bcrypt = require("bcrypt");
-const container = createContainer();
-container
-    .register({
-        createError: asValue(createError),
-        connection: asValue(connection),
-        axios: asValue(axios),
-        jwt: asValue(jwt),
-        config: asValue(config),
-        bcrypt: asValue(bcrypt),
-        app: asValue(app),
-    })
-    .loadModules(
-        [
-            "src/app/operations/**/*.js",
-            "src/app/services/**/*.js",
-            "src/infra/database/repository/**/*.js",
-            "src/infra/integration/rest/**/*.js",
-        ],
-        {
-            formatName: "camelCase",
-            resolverOptions: {
-                injectionMode: InjectionMode.PROXY,
-            },
-        }
-    );
+const container = require("./container");
+const { app, express } = container.cradle;
 app.set("superSecurity-auth", config);
 app.use(cors());
 app.use(morgan("tiny"));
@@ -42,11 +11,18 @@ app.use(scopePerRequest(container));
 app.use(loadControllers("src/interfaces/http/routes/*.js", { cwd: __dirname }));
 app.use(express.json());
 
-const publicDir = require("path").join(
+const pessoaFisica = require("path").join(
     __dirname,
-    "src/interfaces/http/presentations/state/Controllers/state/uploads/"
+    "src/interfaces/http/presentations/people/Controllers/people/uploads/"
 );
-app.use(express.static(publicDir));
+
+const pessoaJuridica = require("path").join(
+    __dirname,
+    "src/interfaces/http/presentations/company/Controllers/company/uploads/"
+);
+
+app.use(express.static(pessoaFisica));
+app.use(express.static(pessoaJuridica));
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({

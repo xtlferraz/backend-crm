bfe23d13c369126fe800cb01477a4487114bb87f
Thiago <thiago.ferraz@dbccompany.com.br>
2020-06-26 15:50:35 -0300

    ajust

--- src/domain/entitities/User.js
+++ src/domain/entitities/User.js
@@ -2,7 +2,7 @@ module.exports = class {
     constructor(id, name, email, password, active) {
         this.id = id;
         this.name = name;
-        this.email = trim(email);
+        this.email = email.trim();
         this.password = password;
         this.active = active;
     }

b6b3b2aaf56b5c27d52deef5f4610dfaaeca6ffd
Thiago <thiago.ferraz@dbccompany.com.br>
2020-06-26 13:32:37 -0300

    Ajust em api pessoas

--- src/interfaces/http/presentations/people/Controllers/PeopleController.js
+++ src/interfaces/http/presentations/people/Controllers/PeopleController.js
@@ -16,57 +16,8 @@ const API = ({
         res.send({ status: 200, result: people.data });
     },
     createPeople: async (req, res) => {
-        new formidable.IncomingForm().parse(req, (err, fields, files) => {
-            if (err) {
-                throw err;
-            }
-
-            fs.copyFileSync(
-                files.photograph.path,
-                __dirname + "/people/uploads/" + files.photograph.name,
-                function (err) {
-                    if (err) {
-                        throw err;
-                    }
-                }
-            );
-
-            createPeopleOperation
-                .execute({
-                    id: fields.id,
-                    photograph: files.photograph.name,
-                    name: fields.name,
-                    cpf: fields.cpf,
-                    dtNascimento: fields.dtNascimento,
-                    sexo: fields.sexo,
-                    active: fields.active,
-                    email: fields.email,
-                    user_id: fields.user_id,
-                    short_name: fields.short_name,
-                    cep: fields.cep,
-                    address: fields.address,
-                    object: fields.object,
-                    state: fields.state,
-                    neighborhood: fields.neighborhood,
-                    country: fields.country,
-                    complement: fields.complement,
-                    rg: fields.rg,
-                    cnh: fields.cnh,
-                    dispatching_agency: fields.dispatching_agency,
-                    resume: fields.resume,
-                    about: fields.about,
-                    formation: fields.formation,
-                    institution: fields.institution,
-                    office: fields.office,
-                    hobby: fields.hobby,
-                })
-                .then(function (response) {
-                    res.send({ status: 200, result: response });
-                })
-                .catch(function (e) {
-                    throw e;
-                });
-        });
+        const response = createPeopleOperation.execute(req.body);
+        res.send({ status: 200, result: response });
     },
     updatePeople: async (req, res) => {
         const response = updatePeopleOperation.execute(req.body);
